d80db23a1745dcba157bf36f7c1f6bf5b0d8d1ea
Misael3101 <torres.31012407@gmail.com>
2025-05-23 19:22:39 -0300

    Agregando ProductItem

--- TP4-grupo6/src/components/ProductList.jsx
+++ TP4-grupo6/src/components/ProductList.jsx
@@ -1,11 +1,8 @@
 import React from "react";
+import ProductItem from "./ProductItem";
 
 export default function ProductList({ productos, onEdit, onDelete }) {
   if (productos.length === 0) return <p>No hay productos para mostrar.</p>;
-
-  const calcularPrecioConDescuento = (precio, descuento) => {
-    return (precio - (precio * descuento) / 100).toFixed(2);
-  };
 
   return (
     <table style={{ width: "100%", borderCollapse: "collapse" }}>
@@ -23,25 +20,12 @@ export default function ProductList({ productos, onEdit, onDelete }) {
       </thead>
       <tbody>
         {productos.map((prod) => (
-          <tr key={prod.id} style={{ borderBottom: "1px solid #ccc" }}>
-            <td style={tdStyle}>{prod.id}</td>
-            <td style={tdStyle}>{prod.titulo}</td>
-            <td style={tdStyle}>{prod.descripcion}</td>
-            <td style={tdStyle}>${prod.precio.toFixed(2)}</td>
-            <td style={tdStyle}>{prod.descuento}%</td>
-            <td style={tdStyle}>
-              ${calcularPrecioConDescuento(prod.precio, prod.descuento)}
-            </td>
-            <td style={tdStyle}>{prod.stock}</td>
-            <td style={tdStyle}>
-              <button onClick={() => onEdit(prod)} style={btnEditStyle}>
-                Editar
-              </button>
-              <button onClick={() => onDelete(prod.id)} style={btnDeleteStyle}>
-                Eliminar
-              </button>
-            </td>
-          </tr>
+          <ProductItem
+            key={prod.id}
+            producto={prod}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
         ))}
       </tbody>
     </table>
@@ -51,27 +35,4 @@ export default function ProductList({ productos, onEdit, onDelete }) {
 const thStyle = {
   textAlign: "left",
   padding: 8,
-};
-
-const tdStyle = {
-  padding: 8,
-};
-
-const btnEditStyle = {
-  marginRight: 10,
-  padding: "5px 10px",
-  backgroundColor: "#4caf50",
-  color: "white",
-  border: "none",
-  borderRadius: 3,
-  cursor: "pointer",
-};
-
-const btnDeleteStyle = {
-  padding: "5px 10px",
-  backgroundColor: "#f44336",
-  color: "white",
-  border: "none",
-  borderRadius: 3,
-  opacity: 0.6,
 };
